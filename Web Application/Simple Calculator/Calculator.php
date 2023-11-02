c98e9a3d415efa4c3f882a2a71c9745615cbb90c
SULTANUM MOBIN <119931140+smsultan76@users.noreply.github.com>
2023-11-02 22:01:35 +0600

    Create Calculator.php
    Hare a simple web application for calculate two value a and b.
    We can do addition a,b.
    We can do subtraction a,b.
    We can do multiplication a,b.
    We can do division a,b.
    We can do exponential a,b.

---
+++ Web Application/Simple Calculator/Calculator.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Calculator</title>
+</head>
+<body>
+    <h1>Simple Calculator web application for begainers</h1>
+    <form action="index.php" method="post">
+        a: <input type="number" name="a"><br>
+        b: <input type="number" name="b"><br>
+        select operator: <br>
+        <input type="radio" name="operator[]" value="plus">Addition <br>
+        <input type="radio" name="operator[]" value="minus">Subtraction <br>
+        <input type="radio" name="operator[]" value="multi">Multiplication <br>
+        <input type="radio" name="operator[]" value="div">Division <br>
+        <input type="radio" name="operator[]" value="pow">a^b <br>
+        <button name="submit">Calculate</button><br>
+    </form>
+</body>
+</html>
+    <?php
+        if(isset($_POST["operator"])){
+            $opt = $_POST["operator"];
+            $a = $_POST["a"];
+            $b = $_POST["b"];
+            if($a!=null and $b!=null){
+            foreach($opt as $sign){
+                switch($sign){
+                    case "plus":
+                        echo "<br>Result is = ",$a+$b;
+                        break;
+                    case "minus":
+                        echo "<br>Result is = ",$a-$b;
+                        break;
+                    case "multi":
+                        echo "<br>Result is = ",$a*$b;
+                        break;
+                    case "div":
+                        echo "<br>Result is = ",$a/$b;
+                        break;
+                    case "pow":
+                        echo "<br>Result is = ",$a**$b;
+                        break;
+                }
+            }
+        }
+        else echo"You must be enter two value";
+    }
+        else{
+            echo"<br>You must be select an operator.<br>";
+        }
+    ?>
