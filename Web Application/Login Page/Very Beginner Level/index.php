abc4a4c0f979b404c50ec04838bdcefe5e47c02a
SULTANUM MOBIN <119931140+smsultan76@users.noreply.github.com>
2023-11-05 21:54:00 +0600

    Create index.php
    This is for only beginner.
    Here it's not secure.
    That only for practice.

---
+++ Web Application/Login Page/Very Beginner Level/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Begainer Login</title>
+</head>
+<body>
+    <h2>This is Home page.</h2>
+    <form action="" method="post">
+        <label for="username">Username: <br></label>
+        <input type="username" name="Username" required> <br>
+        <label for="password">Password: <br></label>
+        <input type="password" name="Password" required> <br>
+        <input type="Submit" name="Submit" value="Login"> <br><br>
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_POST["Submit"])){
+        $user = $_POST["Username"];
+        $pass = $_POST["Password"];
+        if($user == "smsultan76" && $pass == "BD123"){
+            header("Location: WC.php");
+        }
+        else{
+            echo "Incorrect username or password.<br>";
+        }
+    }
+    ?>
